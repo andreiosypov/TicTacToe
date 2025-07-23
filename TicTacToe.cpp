6b8987f99d687896b6551bf62a2ec111df5655d8
Andrei Osypov <andreiosypov@protonmail.com>
2025-07-23 08:29:10 -0500

    added user input

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
+#include <utility>
 using namespace std;
+
+void printBoard(char board[3][3]);
+pair<int, int> getUserInput(char board[3][3]);
 
 int main()
 {
@@ -9,6 +13,28 @@ int main()
         {' ', ' ', ' '},
         {' ', ' ', ' '}};
 
+    const char playerX = 'X';
+    const char playerO = 'O';
+    char currentPlayer = playerX;
+    int r = -1;
+    int c = -1;
+
+    for (int i = 0; i < 9; i++)
+    {
+        printBoard(board);
+        cout << "Player is " << currentPlayer << endl;
+        pair<int, int> userInput = getUserInput(board);
+        r = userInput.first;
+        c = userInput.second;
+        board[r][c] = currentPlayer;
+        currentPlayer = (currentPlayer == playerX) ? playerO : playerX;
+    }
+
+    return 0;
+}
+
+void printBoard(char board[3][3])
+{
     cout << "   |   |   " << endl;
     for (int i = 0; i < 3; i++)
     {
@@ -19,6 +45,30 @@ int main()
         }
         cout << "   |   |   " << endl;
     }
+}
 
-    return 0;
+pair<int, int> getUserInput(char board[3][3])
+{
+    int r = -1;
+    int c = -1;
+    while (true)
+    {
+        cout << "Enter r c from 0-2 for row and column: ";
+        cin >> r >> c;
+        if (r < 0 || r > 2 || c < 0 || c > 2)
+        {
+            cout << "Invalid input, try again." << endl;
+        }
+        else if (board[r][c] != ' ')
+        {
+            cout << "Tile is full, try again." << endl;
+        }
+        else
+            break;
+        r = -1;
+        c = -1;
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+    return {r, c};
 }
